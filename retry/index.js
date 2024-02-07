58e3ac276f1b9a4e5bce25b015163280590bf3f0
anderson-arrieta <Anderson.Arrieta@GlobalBank.com.pa>
2024-02-07 16:56:42 -0500

    create kafka producer

--- retry/index.js
+++ retry/index.js
@@ -1,10 +1,17 @@
-const { messageUSeCase } = require('./application/config/Configuration')
+//const { messageUSeCase } = require('./application/config/Configuration')
+const MessageClient = require('./infrastructure/client/MessageClient');
+const MessageProducer = require('./infrastructure/producer/MessageProducer');
 
 module.exports = async function (context, req) {
     
-    const message = await messageUSeCase.getMessage();
+    //const message = await messageUSeCase.getMessage();
+    const endpoint = 'http://localhost:8082/message';
+    const messageClient = new MessageClient(endpoint);
+    const response = await messageClient.getMessage();
+    const messageProducer = new MessageProducer();
+    messageProducer.emitEvent(endpoint, response);
 
     context.res = {
-        body: message
+        body: response
     };
 }
--- retry/infrastructure/producer/MessageProducer.js
+++ retry/infrastructure/producer/MessageProducer.js
@@ -1,3 +1,42 @@
+const kafka = require('kafka-node');
+
+const kafkaClientOpt = {
+    kafkaHost: 'localhost:9092'
+};
+
+const producerOpt = {
+    requireAcks: 1
+};
+
+const topic = 'message-topic';
+
 class MessageProducer {
-    
+    constructor() {
+        this.producer = new kafka.Producer(new kafka.KafkaClient(kafkaClientOpt), producerOpt);
+    }
+
+    emitEvent(endpoint, response) {
+        const payload = {
+            endpoint,
+            response,
+            timestamp: Date.now()
+        };
+
+        const message = [
+            {
+                topic: topic,
+                message: JSON.stringify(payload)
+            }
+        ];
+
+        this.producer.send(message, (error, data) => {
+            if (error) {
+                console.error('Error sending event:', error);
+            } else {
+                console.log('The event was sent succesfully: ', data);
+            }
+        });
+    }
 }
+
+module.exports = MessageProducer;
